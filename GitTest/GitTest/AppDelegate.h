9acd0f9194d9a30c71b15c323418147315d3bafa
icber <yuanqiuleios@163.com>
2018-01-04 11:13:21 +0800

    第一次提交
    提交

---
+++ GitTest/GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by yy on 2018/1/4.
+//  Copyright © 2018年 yy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTest/GitTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GitTest
+//
+//  Created by yy on 2018/1/4.
+//  Copyright © 2018年 yy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
